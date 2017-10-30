739de53e60fc2302c8dcfe9a03c26313253ac301
Brian Fan <kuomu.fan@yahoo.com>
2017-10-30 13:14:28 -0700

    add handleSubmit redux form function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -41,9 +41,15 @@ class PostsNew extends Component{
         );
     }
 
+    onSubmit(values){
+        console.log(values);
+    }
+
     render(){
+        const { handleSubmit } = this.props;
+
         return(
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field 
                     label="Title"
                     name="title"

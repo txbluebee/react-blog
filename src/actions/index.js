c67fbfd231e7f278469da945b8966dd27da47739
Brian Fan <kuomu.fan@yahoo.com>
2017-10-30 10:49:31 -0700

    install axios tool and create fetchPosts function

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const POST_URL = 'http://reduxblog.herokuapp.com/api'
+const API_KEY = '?key=lily1234'
+
+const FETCH_POSTS = 'fetch_posts';
+
+export function fetchPosts(){
+    const url = `${POST_URL}/posts${API_KEY}`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    }
+}

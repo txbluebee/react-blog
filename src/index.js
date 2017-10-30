ee418577bb229dcb810a58aeaa934a779d413561
Brian Fan <kuomu.fan@yahoo.com>
2017-10-30 10:33:03 -0700

    add postsIndex component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise'
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={PostsIndex} />
+      </Switch> 
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
